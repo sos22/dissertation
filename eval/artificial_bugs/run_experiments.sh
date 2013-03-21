6cf7278d31fa802119182a0a5bbc19ce574261a2
Steven Smith <sos22@cam.ac.uk>
2013-03-21 11:35:57 +0000

    Finish off work on the Thunderbird bug.

--- eval/artificial_bugs/run_experiments.sh
+++ eval/artificial_bugs/run_experiments.sh
@@ -8,8 +8,10 @@ harness=${prefix}/harness
 #cat buglist | while read nr name; do ${harness} -r -n 100 ${prefix}/bug${nr}~0.interp.so ${prefix}/bug${nr}.exe > ${name}~0.crash_times_data ; done
 #cat buglist | while read nr name; do ${harness} -n 100 '<dc>' ${prefix}/bug${nr}.exe > ${name}~0.dc.crash_times_data ; done
 
-SOS22_DELAY_TX=1 ${harness} -l special/crash_indexed_toctou.delay_send.crash_times_data -r -n 100 ${prefix}/bug3~0.interp.so ${prefix}/bug3.exe
-SOS22_DELAY_RX=1 ${harness} -l special/crash_indexed_toctou.delay_recv.crash_times_data -r -n 100 ${prefix}/bug3~0.interp.so ${prefix}/bug3.exe
-SOS22_DELAY_TX=1 ${harness} -l special/interfering_indexed_toctou.delay_send.crash_times_data -r -n 100 ${prefix}/bug4~0.interp.so ${prefix}/bug4.exe
-SOS22_DELAY_RX=1 ${harness} -l special/interfering_indexed_toctou.delay_recv.crash_times_data -r -n 100 ${prefix}/bug4~0.interp.so ${prefix}/bug4.exe
+#SOS22_DELAY_TX=1 ${harness} -l special/crash_indexed_toctou.delay_send.crash_times_data -r -n 100 ${prefix}/bug3~0.interp.so ${prefix}/bug3.exe
+#SOS22_DELAY_RX=1 ${harness} -l special/crash_indexed_toctou.delay_recv.crash_times_data -r -n 100 ${prefix}/bug3~0.interp.so ${prefix}/bug3.exe
+SOS22_DELAY_ALWAYS=1 ${harness} -l special/crash_indexed_toctou.delay_both.crash_times_data -r -n 100 ${prefix}/bug3~0.interp.so ${prefix}/bug3.exe
+#SOS22_DELAY_TX=1 ${harness} -l special/interfering_indexed_toctou.delay_send.crash_times_data -r -n 100 ${prefix}/bug4~0.interp.so ${prefix}/bug4.exe
+#SOS22_DELAY_RX=1 ${harness} -l special/interfering_indexed_toctou.delay_recv.crash_times_data -r -n 100 ${prefix}/bug4~0.interp.so ${prefix}/bug4.exe
+SOS22_DELAY_ALWAYS=1 ${harness} -l special/interfering_indexed_toctou.delay_both.crash_times_data -r -n 100 ${prefix}/bug4~0.interp.so ${prefix}/bug4.exe
 
