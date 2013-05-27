ac6b8cb31502d2db71032d957955756ae801276b
Steven Smith <sos22@srcf.ucam.org>
2013-05-27 15:47:45 +0100

    Check in state of the world.

--- eval/rebuild_graphs.sh
+++ eval/rebuild_graphs.sh
@@ -4,13 +4,13 @@ set -e
 set -x
 
 cd artificial_bugs
-# ./indexed_toctou_no_scs.py > special/indexed_toctou_no_scs.tex
-# ./indexed_toctou_vary_nr_ptrs.py enforcer > special/indexed_toctou_vary_nr_ptrs_enforcer.tex
-# ./indexed_toctou_vary_nr_ptrs.py no_enforcer > special/indexed_toctou_vary_nr_ptrs_no_enforcer.tex
-# ./delay_positioning.py > special/delay_positioning.tex
-# ./multi_bugs.py > special/multi_bugs.tex
-# ./multi_threads.py > special/multi_threads.tex
-# ./crash_time_to_cdf.py > crash_time_cdfs.tex
+./indexed_toctou_no_scs.py > special/indexed_toctou_no_scs.tex
+./indexed_toctou_vary_nr_ptrs.py enforcer > special/indexed_toctou_vary_nr_ptrs_enforcer.tex
+./indexed_toctou_vary_nr_ptrs.py no_enforcer > special/indexed_toctou_vary_nr_ptrs_no_enforcer.tex
+./delay_positioning.py > special/delay_positioning.tex
+./multi_bugs.py > special/multi_bugs.tex
+./multi_threads.py > special/multi_threads.tex
+./crash_time_to_cdf.py > crash_time_cdfs.tex
 cd ..
 
 cd complex_hb
@@ -19,10 +19,10 @@ cd ..
 
 cd alpha
 cd unopt
-#../build_bpm_graph.py > bpm.tex
-#../build_gsc_graph.py > gsc.tex
-#../build_gvc_graph.py > gvc.tex
-#../build_crashing_size_graph.py > crashing_size.tex
+../build_bpm_graph.py > bpm.tex
+../build_gsc_graph.py > gsc.tex
+../build_gvc_graph.py > gvc.tex
+../build_crashing_size_graph.py > crashing_size.tex
 cd ..
 
 cd opt
@@ -30,9 +30,9 @@ cd opt
 # ../build_gsc_graph.py opt > gsc.tex
 # ../build_gvc_graph.py > gvc.tex
 # ../build_crashing_size_graph.py > crashing_size.tex
-# cd ..
+cd ..
 cd ..
 
 cd bubble_charts
-# ./rebuild.sh
+./rebuild.sh
 cd ..
